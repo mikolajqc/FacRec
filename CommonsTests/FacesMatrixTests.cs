15d7a3a778419ed69085c1de419fb894e8d679ae
Mikolaj <mikolajqc@gmail.com>
2017-12-23 01:36:45 +0100

    added CommonTests with FacesMatrixTests

---
+++ CommonsTests/FacesMatrixTests.cs
@@ -0,0 +1,26 @@
+﻿using Commons.Utilities;
+using NUnit.Framework;
+
+namespace CommonsTests
+{
+    [TestFixture]
+    public class FacesMatrixTests
+    {
+
+        [Test]
+        public void CheckContentConstructor()
+        {
+            double[,] testContent = {{1, 2}, {3, 4}};
+            FacesMatrix facesMatrix = new FacesMatrix(testContent);
+            Assert.AreEqual(facesMatrix.Content, testContent);
+        }
+
+        [Test]
+        public void GetAverageVectorTest()
+        {
+            var facesMatrix = new FacesMatrix();
+            facesMatrix.Content = new double[,] { { 1,2}, { 3,4} };
+            Assert.AreEqual(facesMatrix.GetAverageVector(1).GetVectorAsArray(0,1), new double[] {2, 3});
+        }
+    }
+}
